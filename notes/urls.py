2d22dab4f7abb6db50d5005af02de257afba42eb
Rodrigo Buniac <rodrigofb6@al.insper.edu.br>
2026-09-08 09:12:36 -0300

    Tarefa 2 concluida

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<int:note_id>/confirmar', views.delete, name='delete'),
     path('notes/<int:note_id>/edit/', views.edit, name='edit'),
+    path('tags/', views.tags, name='tags'),
+    path('tags/<int:tag_id>/', views.tag_detail, name='tag_detail'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,28 +1,54 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Note
+from django.shortcuts import render, redirect
+from .models import Note, Tag
 
 
 def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
-        Note.objects.create(title=title, content=content)
+        tag_name = request.POST.get('tag')
+
+        tag = None
+        if tag_name:
+            tag, criada = Tag.objects.get_or_create(name=tag_name)
+
+        Note.objects.create(title=title, content=content, tag=tag)
         return redirect('index')
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
 
 def delete(request, note_id):
     note = Note.objects.get(id=note_id)
     note.delete()
     return redirect('index')
 
+
 def edit(request, note_id):
     note = Note.objects.get(id=note_id)
     if request.method == 'POST':
         note.title = request.POST.get('titulo')
         note.content = request.POST.get('detalhes')
+
+        tag_name = request.POST.get('tag')
+        if tag_name:
+            tag, criada = Tag.objects.get_or_create(name=tag_name)
+            note.tag = tag
+        else:
+            note.tag = None
+
         note.save()
         return redirect('index')
     else:
         return render(request, 'notes/edit.html', {'note': note})
+
+def tags(request):
+    all_tags = Tag.objects.all()
+    return render(request, 'notes/tags.html', {'tags': all_tags})
+
+
+def tag_detail(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    notes = Note.objects.filter(tag=tag)
+    return render(request, 'notes/tag_detail.html', {'tag': tag, 'notes': notes})
