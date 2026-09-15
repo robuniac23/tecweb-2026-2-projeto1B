4173ab897b1d83aa28e7419682a9fe182195085a
Rodrigo Buniac <rodrigofb6@al.insper.edu.br>
2026-09-15 08:18:55 -0300

    Tarefa 3

--- getit/settings.py
+++ getit/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Rodrigo33',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
